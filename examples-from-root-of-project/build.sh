fe9d15e5b8704defab2b267f678e48e54b61a522
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-09-03 23:11:44 +1200

    PATCH: improve stuff

--- examples-from-root-of-project/build.sh
+++ examples-from-root-of-project/build.sh
@@ -7,14 +7,14 @@ cd -
 sudo rm themes/mytheme/dist*.gz -rf
 
 # sudo rm themes/mytheme/dist/vendors~app.js -rf
-gzip themes/mytheme/dist/app.css themes/sun_dist/app.css.gz
-gzip themes/mytheme/dist/app.js themes/sun_dist/app.js.gz
+gzip themes/mytheme/dist/app.css        themes/mytheme/dist/app.css.gz
+gzip themes/mytheme/dist/app.js         themes/mytheme/dist/app.js.gz
 gzip themes/mytheme/dist/vendors~app.js themes/mytheme/dist/vendors~app.js.gz
 
 # gzip mytheme for extra fast delivery
-sudo rm vendor/myvendor/mypackage/dist*.gz -rf
+sudo rm vendor/myvendor/mypackage/client/dist*.gz -rf
 
 # sudo rm vendor/myvendor/mypackage/client/dist/vendors~app.js -rf
-gzip vendor/myvendor/mypackage/client/dist/app.css themes/sun_dist/app.css.gz
-gzip vendor/myvendor/mypackage/client/dist/app.js themes/sun_dist/app.js.gz
-gzip vendor/myvendor/mypackage/client/dist/vendors~app.js vendor/myvendor/mypackage/client/dist/vendors~app.js.gz
+gzip vendor/myvendor/mypackage/client/dist/app.css           vendor/myvendor/mypackage/client/dist/app.css.gz
+gzip vendor/myvendor/mypackage/client/dist/app.js            vendor/myvendor/mypackage/client/dist/app.js.gz
+gzip vendor/myvendor/mypackage/client/dist/vendors~app.js    vendor/myvendor/mypackage/client/dist/vendors~app.js.gz
