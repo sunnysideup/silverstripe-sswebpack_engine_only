a764460f2a53229ac55680886a4f490e40e269c3
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-11-09 12:55:50 +1300

    MINOR: build

--- examples-from-root-of-project/build.sh
+++ examples-from-root-of-project/build.sh
@@ -10,6 +10,12 @@ gzip themes/mytheme/dist/app.css        themes/mytheme/dist/app.css.gz
 gzip themes/mytheme/dist/app.js         themes/mytheme/dist/app.js.gz
 gzip themes/mytheme/dist/vendors~app.js themes/mytheme/dist/vendors~app.js.gz
 
+cd themes/mytheme
+git add . -A
+git commit . -m "MINOR: build"
+git push
+cd -
+
 #############################
 # my theme
 #############################
@@ -23,3 +29,9 @@ sudo rm vendor/myvendor/mypackage/client/dist/*.gz -rf
 # sudo rm vendor/myvendor/mypackage/client/dist/vendors~app.js -rf
 gzip vendor/myvendor/mypackage/client/dist/app.css           vendor/myvendor/mypackage/client/dist/app.css.gz
 gzip vendor/myvendor/mypackage/client/dist/app.js            vendor/myvendor/mypackage/client/dist/app.js.gz
+
+cd vendor/myvendor/mypackage/
+git add . -A
+git commit . -m "MINOR: build"
+git push
+cd -
