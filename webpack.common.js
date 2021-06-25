08036880617852944cb4752b268b427f3ab00b83
Nicolaas <github@sunnysideup.co.nz>
2021-06-25 19:26:54 +1200

    WIP

--- webpack.common.js
+++ webpack.common.js
@@ -90,174 +90,173 @@ let customConfig = {}
 if (WEBPACK_CUSTOM_ADD_PATH_CONFIG) {
   customConfig = require(WEBPACK_CUSTOM_ADD_PATH)
 }
-if (HAS_VUE) {
-}
 const { VueLoaderPlugin } = require('vue-loader')
 
-let myConfig = merge({
-  // webpack cache system
-  cache: {
-    type: 'filesystem'
-  },
-  entry: {
-    app: [
-      JS_FILE,
-      CSS_FILE
-    ]
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(DIST_DIR)
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        // exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/react',
-              {
-                plugins: [
-                  '@babel/plugin-proposal-class-properties'
-                ]
-              }
-            ], // Preset used for env setup
-            plugins: [
-              ['@babel/transform-react-jsx']
-            ],
-            cacheDirectory: true,
-            cacheCompression: true
+let myConfig = merge(
+  {
+    // webpack cache system
+    cache: {
+      type: 'filesystem'
+    },
+    entry: {
+      app: [
+        JS_FILE,
+        CSS_FILE
+      ]
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(DIST_DIR)
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          // exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/react',
+                {
+                  plugins: [
+                    '@babel/plugin-proposal-class-properties'
+                  ]
+                }
+              ], // Preset used for env setup
+              plugins: [
+                ['@babel/transform-react-jsx']
+              ],
+              cacheDirectory: true,
+              cacheCompression: true
+            }
           }
-        }
-      },
-      {
-        test: /\.s?css$/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader
         },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: false
-            }
+        {
+          test: /\.s?css$/,
+          use: [{
+            loader: MiniCssExtractPlugin.loader
           },
-          {
-            loader: 'resolve-url-loader'
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              postcssOptions: {
-                plugins: ['autoprefixer']
-              }
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|webp|jpg|jpeg|gif|svg)$/,
-        use: [{
-          loader: 'img-optimize-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: IMG_DIR_CONFIG,
-            compress: {
-            // This will take more time and get smaller images.
-              mode: 'low', // 'lossless', 'high', 'low'
-              disableOnDevelopment: true,
-            // convert to webp
-              webp: false,
-            // loseless compression for png
-              optipng: {
-                optimizationLevel: 4
-              },
-            // lossy compression for png. This will generate smaller file than optipng.
-              pngquant: {
-                quality: [0.2, 0.8]
-              },
-            // Compression for svg.
-              svgo: true,
-            // Compression for gif.
-              gifsicle: {
-                optimizationLevel: 3
-              },
-            // Compression for jpg.
-              mozjpeg: {
-                progressive: true,
-                quality: 60
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: false
               }
             },
-            inline: {
-              limit: 1
+            {
+              loader: 'resolve-url-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                postcssOptions: {
+                  plugins: ['autoprefixer']
+                }
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
             }
-          }
-        }]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            outputPath: FONTS_DIR_CONFIG,
-            name: '[name].[ext]'
-          }
-        }]
-      }
-    ]
-  },
-
-  // extra settings
-  resolve: {
-
-    // defines root folders for compatibility
-    roots: [
-      path.resolve('./../../'),
-      path.resolve('./../../public')
-    ],
-
-    // //node modules to include
-    modules: [
-      path.join(__dirname, 'node_modules'),
-      path.resolve(NODE_DIR)
-    ],
-
-    // aliases
-    alias: {
-      'window.jQuery': require.resolve('jquery'),
-      $: require.resolve('jquery'),
-      jQuery: require.resolve('jquery'),
-
-      // react: require.resolve('react'),
-      // 'react-dom': require.resolve('react-dom'),
-
-      site: path.resolve('./../../'),
-      PROJECT_ROOT_DIR: path.resolve('./../../')
+          ]
+        },
+        {
+          test: /\.(png|webp|jpg|jpeg|gif|svg)$/,
+          use: [{
+            loader: 'img-optimize-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: IMG_DIR_CONFIG,
+              compress: {
+              // This will take more time and get smaller images.
+                mode: 'low', // 'lossless', 'high', 'low'
+                disableOnDevelopment: true,
+              // convert to webp
+                webp: false,
+              // loseless compression for png
+                optipng: {
+                  optimizationLevel: 4
+                },
+              // lossy compression for png. This will generate smaller file than optipng.
+                pngquant: {
+                  quality: [0.2, 0.8]
+                },
+              // Compression for svg.
+                svgo: true,
+              // Compression for gif.
+                gifsicle: {
+                  optimizationLevel: 3
+                },
+              // Compression for jpg.
+                mozjpeg: {
+                  progressive: true,
+                  quality: 60
+                }
+              },
+              inline: {
+                limit: 1
+              }
+            }
+          }]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: FONTS_DIR_CONFIG,
+              name: '[name].[ext]'
+            }
+          }]
+        }
+      ]
     },
 
-    fallback: {
-      path: false
-    }
-  }
+    // extra settings
+    resolve: {
 
-  // in case you load it from CDN
-  /* externals: {
-                  jquery: 'jQuery',
-                  react: 'React',
-                  'react-dom': 'ReactDOM',
-              }, */
-},
-customConfig
+      // defines root folders for compatibility
+      roots: [
+        path.resolve('./../../'),
+        path.resolve('./../../public')
+      ],
+
+      // //node modules to include
+      modules: [
+        path.join(__dirname, 'node_modules'),
+        path.resolve(NODE_DIR)
+      ],
+
+      // aliases
+      alias: {
+        'window.jQuery': require.resolve('jquery'),
+        $: require.resolve('jquery'),
+        jQuery: require.resolve('jquery'),
+
+        // react: require.resolve('react'),
+        // 'react-dom': require.resolve('react-dom'),
+
+        site: path.resolve('./../../'),
+        PROJECT_ROOT_DIR: path.resolve('./../../')
+      },
+
+      fallback: {
+        path: false
+      }
+    }
+
+    // in case you load it from CDN
+    /* externals: {
+                    jquery: 'jQuery',
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                }, */
+  },
+  customConfig
 )
 if (HAS_VUE) {
   myConfig = merge(
