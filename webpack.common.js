b9360f109b04c8dfd4aba69a1fc9b24b9dee8a1e
Nicolaas <github@sunnysideup.co.nz>
2023-01-25 14:24:52 +1300

    PATCH: fixing images

--- webpack.common.js
+++ webpack.common.js
@@ -145,60 +145,12 @@ const myConfig = merge({
             loader: 'resolve-url-loader'
           },
           {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              postcssOptions: {
-                plugins: ['autoprefixer']
-              }
-            }
-          },
-          {
             loader: 'sass-loader',
             options: {
               sourceMap: true
             }
           }
         ]
-      },
-      {
-        test: /\.(png|webp|jpg|jpeg|gif|svg)$/,
-        use: [{
-          loader: 'img-optimize-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: IMG_DIR_CONFIG,
-            compress: {
-              // This will take more time and get smaller images.
-              mode: 'low', // 'lossless', 'high', 'low'
-              disableOnDevelopment: true,
-              // convert to webp
-              webp: false,
-              // loseless compression for png
-              optipng: {
-                optimizationLevel: 4
-              },
-              // lossy compression for png. This will generate smaller file than optipng.
-              pngquant: {
-                quality: [0.2, 0.8]
-              },
-              // Compression for svg.
-              svgo: true,
-              // Compression for gif.
-              gifsicle: {
-                optimizationLevel: 3
-              },
-              // Compression for jpg.
-              mozjpeg: {
-                progressive: true,
-                quality: 60
-              }
-            },
-            inline: {
-              limit: 1
-            }
-          }
-        }]
       },
       {
         test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
